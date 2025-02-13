81ce1185d6917cb33b0dfffe2c416dccef893fae
xiaotan <2653591683@qq.com>
2025-02-14 00:41:51 +0800

    initial

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 import cesium from 'vite-plugin-cesium'
 
 export default defineConfig({
-  plugins: [vue(), cesium()],
+  plugins: [
+    vue(),
+    cesium(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
 })
