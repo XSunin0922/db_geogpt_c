b448b1c34190c622d94ec141430ec1c6ceee18ec
tanlx <tlx2653591683@gmail.com>
2025-02-14 17:27:42 +0800

    initial1

---
+++ src/api/fetchData.js
@@ -0,0 +1,5 @@
+import server from "./axios.js";
+
+export const getXZQExtent = async () => {
+  return await server.get(``);
+}
--- src/components/Map.vue
+++ src/components/Map.vue
@@ -4,10 +4,11 @@ import * as Cesium from 'cesium'
 
 const viewer = ref(null)
 
-const viewerSet = () => {
+const viewerInitial = () => {
   viewer.value = new Cesium.Viewer('mapContainer', {
     animation: false,
     baseLayerPicker: false,
+    fullscreenButton: false,
     geocoder: false,
     homeButton: false,
     sceneModePicker: false,
@@ -18,14 +19,27 @@ const viewerSet = () => {
     requestRenderMode: true,
   });
   viewer.value._cesiumWidget._creditContainer.style.display = "none";
+  // 相机限制
   viewer.value.scene.camera.setView({
-    destination: Cesium.Cartesian3.fromDegrees(114, 30, 8000000)
+    destination: Cesium.Rectangle.fromDegrees(73.66, 3.86, 135.05, 53.55),
   });
+  viewer.value.scene.screenSpaceCameraController.minimumZoomDistance = 100000;
+  viewer.value.scene.screenSpaceCameraController.maximumZoomDistance = 5000000;
+  viewer.value.scene.screenSpaceCameraController.enableTilt = false;
+  viewer.value.scene.screenSpaceCameraController.enableLook = false;
+  // 节约渲染资源
+  viewer.value.scene.skyBox.show = false;
+  viewer.value.scene.skyAtmosphere.show = false;
+  viewer.value.scene.sun.show = false;
+  viewer.value.scene.moon.show = false;
+  viewer.value.scene.fog.enabled = false;
+  viewer.value.scene.globe.showGroundAtmosphere = false;
+  viewer.value.resolutionScale = 0.9;
 }
 
 onMounted(() => {
   Cesium.Ion.defaultAccessToken = import.meta.env.VITE_CESIUM_ACCESS_TOKEN
-  viewerSet()
+  viewerInitial()
 })
 </script>
 
