04768dcf92dd6fedfd971345286a55fe722f5aa5
tanlx <tlx2653591683@gmail.com>
2025-02-18 17:42:24 +0800

    18day

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
+import {createPinia} from "pinia";
 import './style.css'
 import App from './App.vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
 }
+app.use(pinia)
 app.mount('#app')
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import {Notebook, User} from "@element-plus/icons-vue";
+import Map from "../components/Map.vue";
+import left from "../views/left.vue";
+import right from "../views/right.vue";
+import bottom from "../views/bottom.vue";
+import {useStatusStore} from "../store/status.js";
 
-
+const statusStore = useStatusStore()
 </script>
 
 <template>
@@ -11,19 +16,19 @@ import {Notebook, User} from "@element-plus/icons-vue";
       <span class="title">中国经济统计数据库</span>
       <div class="icons">
         <div class="icon">
-          <el-icon><Notebook /></el-icon>
+          <el-icon size="16"><Notebook /></el-icon>
           <span>日志</span>
         </div>
         <div class="icon">
-          <el-icon><User /></el-icon>
+          <el-icon size="16"><User /></el-icon>
           <span>用户</span>
         </div>
       </div>
     </div>
     <div class="body">
-      <div class="left_column"></div>
+      <div class="left_column"><left /></div>
       <div class="middle_column">
-        <div class="map"></div>
+        <div class="map"><Map /></div>
         <div class="bottom"></div>
       </div>
       <div class="right_column"></div>
@@ -39,12 +44,14 @@ import {Notebook, User} from "@element-plus/icons-vue";
   display: flex;
   flex-direction: column;
   .header {
+    user-select: none;
     position: relative;
     height: 60px;
     background-color: #1F2B3D;
     display: flex;
     flex-wrap: wrap;
     align-content: center;
+    justify-content: space-between;
     font-family: "PingFang SC", sans-serif;
     .title {
       color: #FFFFFF;
@@ -54,22 +61,20 @@ import {Notebook, User} from "@element-plus/icons-vue";
       letter-spacing: 3px;
     }
     .icons {
-      position: absolute;
-      right: 20px;
+      margin-right: 20px;
       display: flex;
+      flex-wrap: wrap;
       align-content: center;
+      gap: 10px;
       .icon {
-        margin-left: 20px;
-        display: flex;
+        display: inline-flex;
         flex-direction: column;
         align-items: center;
+        gap: 2px;
         color: #FFFFFF;
-        font-size: 20px;
+        font-size: 12px;
         font-weight: 500;
         letter-spacing: 2px;
-        span {
-          margin-left: 5px;
-        }
       }
     }
   }
@@ -77,24 +82,28 @@ import {Notebook, User} from "@element-plus/icons-vue";
     flex: 1;
     display: flex;
     .left_column {
-      width: 200px;
-      background-color: #f0f0f0;
+      width: 350px;
+      height: 100%;
     }
     .middle_column {
+      height: 100%;
+      width: calc(100% - 800px);
       flex: 1;
       display: flex;
       flex-direction: column;
+      background-color: #f0f0f0;
+
       .map {
-        flex: 1;
-        background-color: #f0f0f0;
+        height: 600px;
       }
       .bottom {
-        height: 200px;
+        height: calc(100% - 600px);
         background-color: #f0f0f0;
       }
     }
     .right_column {
-      width: 200px;
+      height: 100%;
+      width: 450px;
       background-color: #f0f0f0;
     }
   }
